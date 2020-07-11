29fd834034ff8995e2f2a36a779f61b4c98b0e4b
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-07-11 17:50:31 +0530

    Create removeDuplicates.js

---
+++ strings/removeDuplicates.js
@@ -0,0 +1,10 @@
+function removeDuplicates(string) {
+  if (string.length === 1) {
+    return string;
+  } else if (string[0] === string[1]) {
+    return removeDuplicates(string.substr(1));
+  } else {
+    return string[0] + removeDuplicates(string.substr(1));
+  }
+}
+console.log(removeDuplicates("helloo"));
